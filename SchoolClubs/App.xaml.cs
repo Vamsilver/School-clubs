7a70072326a8aacb0b2a2f8fe6298afc5cec77dd
steelbsteel <mrtrecer@bk.ru>
2023-10-17 19:49:08 +0300

    FirstCommit

--- SchoolClubs/App.xaml.cs
+++ SchoolClubs/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolClubs.BD;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace SchoolClubs
     /// </summary>
     public partial class App : Application
     {
+        public static SchoolEntities connection = new SchoolEntities();
     }
 }
--- SchoolClubs/Pages/Teacher/TeacherHomePage.xaml.cs
+++ SchoolClubs/Pages/Teacher/TeacherHomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolClubs.BD;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,14 +22,21 @@ namespace SchoolClubs.Pages.Teacher
     /// </summary>
     public partial class TeacherHomePage : Page
     {
-        public TeacherHomePage()
+        User teacher;
+        public TeacherHomePage(User _teacher)
         {
+            teacher = _teacher;
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void TeacherHomePageLoaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = teacher;
         }
     }
 }
